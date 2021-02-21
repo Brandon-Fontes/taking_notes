6ff3a521594a70f7345e0f182086f47da1d597db
DaFonz <brandonjfontes@gmail.com>
2021-02-21 16:06:09 -0700

    added routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,4 +7,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '11-express\02-Homework\Develop\public\notes.html'));
 });
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "11-express\02-Homework\taking_notes\Develop\public\index.html"));
+});
+
 app.listen(8080);
